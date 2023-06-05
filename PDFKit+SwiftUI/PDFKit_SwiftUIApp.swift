002ea036dca0f1bf2dcb45a55bbbc31814af2ff4
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-06-05 16:10:24 +0300

    Initial Commit

---
+++ PDFKit+SwiftUI/PDFKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFKit_SwiftUIApp.swift
+//  PDFKit+SwiftUI
+//
+//  Created by Vasichko Anna on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
